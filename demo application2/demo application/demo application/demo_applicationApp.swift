3cdb21a5c6ec7d158fe349a589663d507a771251
amirsohailpk <139463256+amirsohailpk@users.noreply.github.com>
2023-07-14 12:18:11 +0500

    Add files via upload
    first time

---
+++ demo application2/demo application/demo application/demo_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_applicationApp.swift
+//  demo application
+//
+//  Created by Amir sohail on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct demo_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
